c13e396b803abf6fdd95c684f8f2438297eb08a8
bstabler <benstabler@yahoo.com>
2019-04-14 12:38:09 -0700

    pycodestyle

--- bca4abm/processors/four_step/aggregate_zone.py
+++ bca4abm/processors/four_step/aggregate_zone.py
@@ -30,6 +30,7 @@ Maybe the ma.<purpose|income>dcls.csv files should be added to the
 mf.cval.csv before input to the bca tool?
 """
 
+
 @inject.step()
 def aggregate_zone_processor(
         zones,
@@ -43,10 +44,9 @@ def aggregate_zone_processor(
 
     trace_label = 'aggregate_zone'
     model_settings = config.read_model_settings('aggregate_zone.yaml')
-    
     spec_file_name = model_settings.get('spec_file_name', 'aggregate_zone.csv')
     aggregate_zone_spec = bca.read_assignment_spec(spec_file_name)
-    
+
     zones_df = zones.to_frame()
 
     logger.info("Running aggregate_zone_processor with %d zones"
@@ -96,5 +96,5 @@ def aggregate_zone_benefits(
     model_settings = config.read_model_settings('aggregate_zone.yaml')
     spec_file_name = model_settings.get('spec_file_name', 'aggregate_zone.csv')
     aggregate_zone_spec = bca.read_assignment_spec(spec_file_name)
-    
+
     add_aggregate_results(zone_summary, aggregate_zone_spec, source=trace_label)
